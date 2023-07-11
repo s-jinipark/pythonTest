9279f8311fd405d7c944c0bc72c0f39d6310ad27
Jini Park <jinipark@naver.com>
2023-07-11 23:55:07 +0900

    2023 07 11 home 1

---
+++ inf-learn/section6/연습/06_연습14.py
@@ -0,0 +1,52 @@
+
+##### 인접행렬(가중치 방향그래프)
+
+def DFS(v) :  # 노드 번호
+    if v == N :  # 종착점
+        print('>', ch)
+        for i in range(1, N+1):
+            if ch[i] == 1:
+                print(i, end=' ')
+        print()
+    else :
+        for i in range(1, N+1):   # 1번 부터, 0 아님
+            if ch[i] == 0  and g[v][i] == 1:  # v -> i 로 이동
+                ch[i] = 1
+                DFS(i)
+                ch[i] = 0
+
+def solution():
+    answer = 0
+
+    for i in  inp:
+        a, b = i
+        #print(a,b)
+        g[a][b] = 1
+    print(g)
+
+    ch[1] = 1   # 방문 표시
+    DFS(1)
+
+    return answer
+
+N = 5   # 정점의 수
+M = 9   # 간선의 수
+
+g=[[0]* (N+1)  for _ in range(N+1)]
+ch = [0] * (N+1)  # 0 은 안 씀
+inp = [
+    (1, 2),
+    (1, 3),
+    (1, 4),
+    (2, 1),
+    (2, 3),
+    (2, 5),
+    (3, 4),
+    (4, 2),
+    (4, 5)
+]
+
+re = solution()
+print(re)
+#=>
+print('====================')
